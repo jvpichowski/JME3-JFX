32324e8f5fd5e346b78e2194e94e6931a9c4b52a
Jan von Pichowski <janvonpichowski@gmail.com>
2016-06-06 09:29:48 +0200

    Finished IntroVideo example

--- src/test/java/com/jme3/jfx/example/IntroVideo.java
+++ src/test/java/com/jme3/jfx/example/IntroVideo.java
@@ -18,6 +18,12 @@ import javafx.scene.paint.Color;
 
 /**
  * Created by jan on 17.05.16.
+ *
+ * Shows a video. Behind the video is a blue box.
+ * When the video has finished it'll be removed and you see the blue box.
+ * This example could be used to implemented a loading screen while big
+ * assets are loaded.
+ *
  */
 public class IntroVideo {
 
@@ -26,46 +32,51 @@ public class IntroVideo {
         Application app = new SimpleApplication(jFxManager) {
             @Override
             public void simpleInitApp() {
-                Box b = new Box(1, 1, 1); // create cube shape
-                Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
-                Material mat = new Material(assetManager,
-                        "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
-                mat.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
-                geom.setMaterial(mat);                   // set the cube's material
-                rootNode.attachChild(geom);              // make the cube appear in the scene
-
             }
         };
-        jFxManager.onInit(() -> {
-            jFxManager.launch(Context.create(new Configuration().setViewPort(app.getGuiViewPort())),
-                primaryStage -> {
-                    //Add a scene
-                    Group root = new Group();
-                    Scene scene = new Scene(root, primaryStage.getWidth(), primaryStage.getHeight());
-                    scene.setFill(new Color(0,0,0,1));
-                    //scene.setFill(new Color(0,0,0,0));
+        jFxManager.onInit(() -> jFxManager.launch(Context.create(new Configuration().setViewPort(app.getGuiViewPort())),
+            primaryStage -> {
+                //Add a scene
+                Group root = new Group();
+                Scene scene = new Scene(root, primaryStage.getWidth(), primaryStage.getHeight());
+                scene.setFill(new Color(0,0,0,1));
+                //scene.setFill(new Color(0,0,0,0));
 
-                    final Media media = new Media("http://download.oracle.com/otndocs/products/javafx/oow2010-2.flv");
-                    MediaPlayer player = new MediaPlayer(media);
-                    player.setOnEndOfMedia(() -> app.enqueue(() -> primaryStage.close()));
-                    player.play();
+                final Media media = new Media("http://download.oracle.com/otndocs/products/javafx/oow2010-2.flv");
+                MediaPlayer player = new MediaPlayer(media);
+                player.setOnEndOfMedia(() -> app.enqueue(() -> primaryStage.close()));
+                player.play();
 
-                    //This mediaView is added to a Pane
-                    MediaView mediaView = new MediaView(player);
+                //This mediaView is added to a Pane
+                MediaView mediaView = new MediaView(player);
 
-                    //scale media to fill the stage
-                    mediaView.setFitWidth(primaryStage.getWidth());
+                //scale media to fill the stage
+                mediaView.setFitWidth(primaryStage.getWidth());
 
-                    //center mediaView on screen. Ratio of video is 1:2,5
-                    mediaView.setTranslateY(((double)primaryStage.getHeight()-(1d/2.5d*(double)primaryStage.getWidth()))/2d);
-                    ((Group)scene.getRoot()).getChildren().add(mediaView);
+                //center mediaView on screen. Ratio of video is 1:2,5
+                mediaView.setTranslateY(((double)primaryStage.getHeight()-(1d/2.5d*(double)primaryStage.getWidth()))/2d);
+                ((Group)scene.getRoot()).getChildren().add(mediaView);
 
-                    //show the stage
-                    primaryStage.setTitle("Media Player");
-                    primaryStage.setScene(scene);
-                    primaryStage.show();
+                //show the stage
+                primaryStage.setTitle("Intro");
+                primaryStage.setScene(scene);
+                primaryStage.show();
+
+                app.enqueue(() -> {
+                    //add your assets while the video is running in front
+                    //before you should have loaded your assets in a separate
+                    //thread to not block the jME3 Render Thread, which renders
+                    //the video
+                    Box b = new Box(1, 1, 1);                                   // create cube shape
+                    Geometry geom = new Geometry("Box", b);                     // create cube geometry from the shape
+                    Material mat = new Material(app.getAssetManager(),
+                            "Common/MatDefs/Misc/Unshaded.j3md");               // create a simple material
+                    mat.setColor("Color", ColorRGBA.Blue);                      // set color of material to blue
+                    geom.setMaterial(mat);                                      // set the cube's material
+                    ((SimpleApplication)app).getRootNode().attachChild(geom);   // make the cube appear in the scene
+
                 });
-        });
+            }));
         app.start();
     }
 }
