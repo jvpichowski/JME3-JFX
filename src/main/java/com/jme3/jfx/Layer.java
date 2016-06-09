dc2ad8191ac3f9797ec11f81de75c9802a4152e1
Jan von Pichowski <janvonpichowski@gmail.com>
2016-06-09 13:36:38 +0200

    Added docs

--- src/main/java/com/jme3/jfx/Layer.java
+++ src/main/java/com/jme3/jfx/Layer.java
@@ -23,29 +23,75 @@ public interface Layer {
         throw new IllegalStateException("Not implemented yet");
     }
 
+    /**
+     * Moves this layer to the front of the context
+     */
     void toFront();
+
+    /**
+     * Moves this layer to the background of the context
+     */
     void toBack();
 
     void setPosition(float x, float y);
     void setSize(float width, float height);
 
+    /**
+     *
+     * @param scene
+     */
     void setScene(Scene scene);
 
+    /**
+     * Ungrabs the focus from this layer.
+     * No more key events are handled.
+     */
     void loseFocus();
+
+    /**
+     * Grabs the focus to this layer.
+     * Key events will be handled.
+     */
     void grabFocus();
+
+    /**
+     * Key events are handled if the layer has the focus.
+     *
+     * @return
+     */
     boolean hasFocus();
 
+    /**
+     * Defines when mouse inputs are handled
+     *
+     * @param mode
+     */
     void setInputConsumerMode(BiPredicate<Layer, Point> mode);
 
     //TODO add config param that shows the layer at creating to save a frame
     void show();
+
+    /**
+     * Close this layer. If it belongs to a
+     * forced single layer context the context
+     * will be destroyed to.
+     */
     void close();
 
+    /**
+     *
+     * @return
+     */
     int getWidth();
     int getHeight();
     float getX();
     float getY();
 
+    /**
+     * Sets the title of this layer. YOu can't see this
+     * title. It is only useful for debugging.
+     * @param title
+     */
     void setTitle(String title);
 
 }
